070b70a3d7996fb5b4fc1894df85af1421c200a5
unknown <raphael.pinson@etu.univ-nantes.fr>
2023-10-06 15:39:21 +0200

    TP04 Utilisation de Methodes Statiques

---
+++ src/utils/TestMethodeStatic.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class TestMethodeStatic {
+
+	public static void main(String[] args) {
+		String chaine = "69";
+		int x = Integer.parseInt(chaine);
+		System.out.println(x);
+		
+		int a = 69;
+		int b = 83;
+		x = Integer.max(a, b);
+		System.out.println(x);
+	}
+
+}
