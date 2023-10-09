eda0ca4458e802e91129ce68d7300f3f4a0c4dcd
unknown <raphael.pinson@etu.univ-nantes.fr>
2023-10-09 10:19:57 +0200

    TP05 Encapsulation

---
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,11 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+	private int numCompte;
+	private double solde;
+	
+	public Compte(int numeroCompte,double solde) {
+		this.numCompte = numeroCompte;
+		this.solde = solde;
+	}
+}
---
+++ src/fr/diginamic/banque/entites/TestBanque.java
@@ -0,0 +1,11 @@
+package fr.diginamic.banque.entites;
+
+public class TestBanque {
+
+	public static void main(String[] args) {
+		Compte compte = new Compte(123,12.25);
+		System.out.println(compte);
+
+	}
+
+}
